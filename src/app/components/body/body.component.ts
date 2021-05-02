a4fc8569f75cf562420bf0c972b9ebc4312f2d21
JuansebastianReyes <reyes_garcia1103@hotmail.com>
2021-05-02 15:25:18 -0500

    End Proyect

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
+  comentarios: any[] = [{
+    nombre: "Juan Sebastian Reyes",
+    alias: "@jsreyesg",
+    comentario: "Hola a todos jajajajaja"
+  },
+  {
+    nombre: "Andres Perez",
+    alias: "@aperez",
+    comentario: "Vienvenido al Grupo"
+  }];
+
+  nombre: any;
+  comentario: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+agregarComentarios(){
+  console.log(this.nombre);
+  console.log(this.comentario);
+
+  let comentarioaux ={
+    nombre: this.nombre,
+    alias: `@${this.nombre}`,
+    comentario: this.comentario
+  }
+  this.comentarios.push(comentarioaux);
+}
+
+eliminar(index: any){
+  this.comentarios.splice(index, 1);
+}
 
 }
